fdaa83f226dc264eb0dbf989a5ccd89f2a0b7934
badboyam <pouya001111@gmail.com>
2016-02-22 09:41:18 -0800

    Update left_group.lua

--- plugins/left_group.lua
+++ plugins/left_group.lua
@@ -6,7 +6,7 @@ local bot_id = 182195776 -- your bot id
     if matches[1] == 'leavenod32' and is_admin(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
-      send_large_msg("chat#id"..msg.to.id, 'this is not one of my groups fuck you (coming soon u Globall Banned and Blocked of this bot KOSE NANAT️).', ok_cb, false)
+      send_large_msg("chat#id"..msg.to.id, 'BYR:-).', ok_cb, false)
       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
       block_user("user#id"..msg.from.id,ok_cb,false)
     end
@@ -14,7 +14,7 @@ end
  
 return {
   patterns = {
-    "^[!/](leavenod32)$",
+    "^[!/](leave)$",
     "^!!tgservice (.+)$",
   },
   run = run
